e07054d59e7974b94e5517d5fef4a54e227dcc98
sastriodjamier <sastriodjamier@gmail.com>
2024-07-28 21:41:40 +0700

    add script create db and schema

---
+++ scripts/init-db.sql
@@ -0,0 +1,9 @@
+-- Database for fashion e-commerce
+create database fashion_ecommerce;
+\c fashion_ecommerce
+create schema raw;
+
+-- Database for dota2
+create database dota2;
+\c dota2
+create schema raw;
